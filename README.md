9a9874337705c020f8ec8cc98531256ff0091218
YashwanthPandi <pandiyashwanth@gmail.com>
2024-04-07 11:53:42 -0700

    Remove index.js

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# INDIA_API_PINCODES
+# India Postal Codes API
+
+This Node.js server provides an API for retrieving data about India postal codes and their corresponding geographical information.
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/india-postal-codes-api.git
+    ```
+
+2. Install the dependencies:
+
+    ```bash
+    cd india-postal-codes-api
+    npm install
+    ```
+
+3. Start the server:
+
+    ```bash
+    npm start
+    ```
+
+## Usage
+
+### Get Postal Code Details
+
+Send a GET request to the following endpoint to retrieve details about a specific postal code:
--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/hello',(req,res)=>{
-        res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-async function start() {
-        try {
-            await app.listen(port);
-            console.log(`I'm Connected to Port :${port}`);
-        } catch (err) {
-            console.error(err);
-        }
-}
+const start = async () => {
+  try {
+    console.log(`Application Connected to the Port : ${port}`);
+  } catch (error) {
+    console.error("Error starting application: ", error);
+  }
+};
 
+app.listen(port, () => console.log(`Example app listening on port ${port} !`));
 
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port} !`),
-);
+start();
